ff7c81de4907aa6146a656c2ffa8856e89d62376
oginoapp <mailofogino@gmail.com>
2017-12-29 18:11:34 +0900

    LeftB, RightB, MidB関数の追加

--- CSLibrary/Util/StringUtility.cs
+++ CSLibrary/Util/StringUtility.cs
@@ -94,5 +94,91 @@ namespace CSLibrary.Util
             return str.Substring(str.Length - len, len);
         }
 
+        #region　LeftB メソッド
+
+        /// -----------------------------------------------------------------------------------------
+        /// <summary>
+        ///     文字列の左端から指定したバイト数分の文字列を返します。</summary>
+        /// <param name="stTarget">
+        ///     取り出す元になる文字列。<param>
+        /// <param name="iByteSize">
+        ///     取り出すバイト数。</param>
+        /// <returns>
+        ///     左端から指定されたバイト数分の文字列。</returns>
+        /// -----------------------------------------------------------------------------------------
+        public static string LeftB(string stTarget, int iByteSize)
+        {
+            return MidB(stTarget, 1, iByteSize);
+        }
+
+        #endregion
+
+
+        #region　MidB メソッド (+1)
+
+        /// -----------------------------------------------------------------------------------------
+        /// <summary>
+        ///     文字列の指定されたバイト位置以降のすべての文字列を返します。</summary>
+        /// <param name="stTarget">
+        ///     取り出す元になる文字列。</param>
+        /// <param name="iStart">
+        ///     取り出しを開始する位置。</param>
+        /// <returns>
+        ///     指定されたバイト位置以降のすべての文字列。</returns>
+        /// -----------------------------------------------------------------------------------------
+        public static string MidB(string stTarget, int iStart)
+        {
+            System.Text.Encoding hEncoding = System.Text.Encoding.GetEncoding("Shift_JIS");
+            byte[] btBytes = hEncoding.GetBytes(stTarget);
+
+            return hEncoding.GetString(btBytes, iStart - 1, btBytes.Length - iStart + 1);
+        }
+
+
+        /// -----------------------------------------------------------------------------------------
+        /// <summary>
+        ///     文字列の指定されたバイト位置から、指定されたバイト数分の文字列を返します。</summary>
+        /// <param name="stTarget">
+        ///     取り出す元になる文字列。</param>
+        /// <param name="iStart">
+        ///     取り出しを開始する位置。</param>
+        /// <param name="iByteSize">
+        ///     取り出すバイト数。</param>
+        /// <returns>
+        ///     指定されたバイト位置から指定されたバイト数分の文字列。</returns>
+        /// -----------------------------------------------------------------------------------------
+        public static string MidB(string stTarget, int iStart, int iByteSize)
+        {
+            System.Text.Encoding hEncoding = System.Text.Encoding.GetEncoding("Shift_JIS");
+            byte[] btBytes = hEncoding.GetBytes(stTarget);
+
+            return hEncoding.GetString(btBytes, iStart - 1, iByteSize);
+        }
+
+        #endregion
+
+
+        #region　RightB メソッド
+
+        /// -----------------------------------------------------------------------------------------
+        /// <summary>
+        ///     文字列の右端から指定されたバイト数分の文字列を返します。</summary>
+        /// <param name="stTarget">
+        ///     取り出す元になる文字列。</param>
+        /// <param name="iByteSize">
+        ///     取り出すバイト数。</param>
+        /// <returns>
+        ///     右端から指定されたバイト数分の文字列。</returns>
+        /// -----------------------------------------------------------------------------------------
+        public static string RightB(string stTarget, int iByteSize)
+        {
+            System.Text.Encoding hEncoding = System.Text.Encoding.GetEncoding("Shift_JIS");
+            byte[] btBytes = hEncoding.GetBytes(stTarget);
+
+            return hEncoding.GetString(btBytes, btBytes.Length - iByteSize, iByteSize);
+        }
+
+        #endregion
+
     }
 }
